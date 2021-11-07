b74a4e4a3c8f32e7c8965d3c44340c82dba53ca5
aidnrm <amccmack@gmail.com>
2021-11-07 15:19:21 -0500

    from pc to laptop.  I wrote this on my pc

--- main.cpp
+++ main.cpp
@@ -7,31 +7,44 @@
 #include "bookData.h"
 #include <vector>
 #include <random>
-#include <chrono> // Used for timer
+#include <string>
+#include <cstdio> // used for the timer
+#include <ctime> // used for the timer
 using namespace std;
+using std::to_string;
 int main() {
-    int vectSize = 10; // vector size
+    clock_t  start;
+    double duration;
+    // Two-sort
+    vector<bookData> twoSortBookData;
+    loadFromFile("../books.csv", twoSortBookData);
+    twoSortBookData.resize(1000); // change the vectors size from 3500 to 1000
+
+    start = clock(); // start clock
+    selectionSort(twoSortBookData); // unstable selection sort
+    duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+    cout << "Duration of unstable selection sort " << duration << " seconds" <<'\n'; // record the duration of the sort
+
+    start = clock(); // start clock
+    selectionSortStable(twoSortBookData); // stable selection sort on different field.
+    duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+    cout << "Duration of stable selection sort " << duration << " seconds" <<'\n'; // record the duration of the sort
+
+
+    int vectorSize = 100; // vector size
     vector<bookData> bookDataVec;
     loadFromFile("../books.csv", bookDataVec);
-    bookDataVec.resize(vectSize);
-    heapSort(bookDataVec);
-    // TODO: Each of the five sorting algorithms should be given identical unsorted vectors to begin with
     // Record the number of reads and writes needed to sort a vector of size 100, 200, 300, 400, 500, 600, 700, 800, 900, and 1000
-   /* for(vectSize; vectSize < 1000; vectSize+=10) {
+    for(vectorSize; vectorSize <= 1000; vectorSize+=100) {
         // Create a bookData vector and load books info
         vector<bookData> bookDataVec;
         loadFromFile("../books.csv", bookDataVec);
-        bookDataVec.resize(vectSize);
-        // Put vector of bookData into bubble sort.
+        bookDataVec.resize(vectorSize);
         bubbleSort(bookDataVec);
-        // Put vector of bookData into selection sort.
         selectionSort(bookDataVec);
-        // Put vector of bookData into selection sort.
-        selectionSortGetTitle(bookDataVec);
-        // Put vector of bookData into unstable quick sort.
+        selectionSortStable(bookDataVec);
         quickSortUnstable(bookDataVec);
-        // Put vector of bookData into heap sort.
         heapSort(bookDataVec);
-    }*/
+    }
     return 0;
 }
